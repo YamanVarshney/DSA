2a118e93caa82975a1b248a54c0079e496d545a3
Yaman Varshney <your-github-guptayaman699@gmail.com>
2026-04-09 00:19:05 +0530

    Second commit

---
+++ InsertElement.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class InsertElement{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int arr[] = new int[n+1];
+        for (int i =0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int position = sc.nextInt();
+        int x = sc.nextInt();
+
+        for(int i=n; i>=position; i--){
+            arr[i] = arr[i-1];
+        }
+
+        arr[position-1] = x;
+
+        for(int i=0; i<=n; i++){
+            System.out.print(arr[i]+" ");
+        }
+
+        sc.close();
+    }
+}
