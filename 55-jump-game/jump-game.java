cf761e19603c60d5cff14adfaa93d1d641d3a139
YamanVarshney <guptayaman699@gmail.com>
2026-08-09 19:46:29 +0530

    Time: 3 ms (20.92%) | Memory: 47.6 MB (96.17%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+         int last = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (i > last)
+                return false;
+
+            last = Math.max(last, i + nums[i]);
+        }
+
+        return true;
+    }
+}
