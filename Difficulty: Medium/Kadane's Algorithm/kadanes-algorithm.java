c33490d92791223542b4be1bf5d8564f50ad3ac3
YamanVarshney <guptayaman699@gmail.com>
2026-08-26 22:36:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,18 @@
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        // Code here
+        int max =arr[0];
+        int sum =0;
+        
+        for (int i =0; i<arr.length ; i++){
+            sum = sum+ arr[i];
+            if(sum > max){
+                max = sum;
+            }
+            if(sum< 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
