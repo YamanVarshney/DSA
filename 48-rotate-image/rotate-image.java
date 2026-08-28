f22bdf0ee0bbb3991bea2f8ced72ddbab89e2101
YamanVarshney <guptayaman699@gmail.com>
2026-08-28 20:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (98.37%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int n= arr.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<i; j++){
+                int temp = arr[i][j];
+                arr[i][j] =arr[j][i];
+                arr[j][i] =temp;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            int left =0;
+            int right = n-1;
+
+            while(left<right){
+                int temp= arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i] [right] = temp;
+
+                left++;
+                right--; 
+            }
+        }
+    }
+}
