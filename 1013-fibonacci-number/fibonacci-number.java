05dfc246b64cef1365f61e94263267529aea134f
YamanVarshney <guptayaman699@gmail.com>
2026-08-31 23:06:27 +0530

    Time: 11 ms (9.71%) | Memory: 41.8 MB (93.57%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -11,13 +11,13 @@ class Solution {
         // }
         // return b;
 
-        // if (n==0)
-        // return 0;
+        // // if (n==0)
+        // // return 0;
 
-        // if(n==1)
-        // return 1;
+        // // if(n==1)
+        // // return 1;
 
-        // return fib(n-1) + fib(n-2);
+        // // return fib(n-1) + fib(n-2);
 
         if(n==0 || n==1) return n;
         return fib(n-1) + fib(n-2);
