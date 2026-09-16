2ca2bbfedcf99d19babb635381efa43a75e36517
Yaman Varshney <guptayaman699@gmail.com>
2026-09-16 23:46:04 +0530

    Time: 17 ms (88.22%) | Memory: 108.1 MB (56.99%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,24 @@
+import java .util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    //     for (int i=0 ; i<nums.length; i++){
+    //         for(int j =i+ 1; j<nums.length; j++){
+    //             if (nums[i] == nums[j]){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+    HashSet<Integer> set = new HashSet<>();
+
+    for(int num : nums){
+        if (set.contains(num)){
+            return true;
+        }
+        set.add(num);
+    }
+
+    return false;
+}
+}
